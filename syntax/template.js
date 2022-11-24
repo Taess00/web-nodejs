3fb40e23054f52746982b555bfe0cf64a0c9db4b
Taess00 <89635061+Taess00@users.noreply.github.com>
2022-11-24 21:30:21 +0900

    practice nodejs

---
+++ syntax/template.js
@@ -0,0 +1,6 @@
+var name2 = 'TYss';
+var letter = `Dear ${name2}
+
+hello my name is ${name2}`;
+
+console.log(letter);
---
+++ syntax/variable2.js
@@ -0,0 +1,3 @@
+var egoing = 'name';
+var letter = 'hello my' + egoing + 'is TYss';
+console.log(letter);
